548c06a0a1cd143d9d32b69634160a9f292da38d
cake17 <cake17@cake-websites.com>
2014-11-17 14:34:05 +0100

    add Search Behavior

---
+++ Model/Behavior/SearchBehavior.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Search Behavior
+ *
+ * @author   cake17
+ * @license  http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @link     http://cake17.github.io
+ *
+ */
+namespace Search\Model\Behavior;
+
+use Cake\Event\Event;
+use Cake\ORM\Behavior;
+use Cake\ORM\Query;
+use Cake\ORM\Table;
+
+class SearchBehavior extends Behavior {
+
+/**
+ * $_defaultConfig For the Behavior
+ *
+ * @var array
+ */
+	protected $_defaultConfig = [
+		'implementedFinders' => [
+			'search' => 'findSearch'
+		],
+	];
+
+/**
+ * Callback fired from the controller
+ *
+ * @param Query $query : Query
+ * @param array $options : The GET arguments
+ * @return \Cake\ORM\Query : The Query object used in pagination
+ */
+	public function findSearch(Query $query, array $options) {
+		foreach ($this->_table->searchConfiguration()->all() as $config) {
+			$config->args($options);
+			$config->query($query);
+			$config->process();
+		}
+
+		return $query;
+	}
+
+}
