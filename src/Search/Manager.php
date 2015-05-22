5846af144478034c02570ddf6bdb7c95f642cb5f
Florian Krämer <florian.kraemer@cakedc.com>
2015-05-22 10:45:06 +0200

    Started implementing the proposal https://github.com/FriendsOfCake/search/issues/16

--- src/Search/Manager.php
+++ src/Search/Manager.php
@@ -15,6 +15,10 @@ class Manager
      */
     protected $_table;
 
+    protected $_filters = [];
+
+    protected $_collection = 'default';
+
     /**
      * Config
      *
@@ -42,7 +46,7 @@ class Manager
      */
     public function all()
     {
-        return $this->_config['types'];
+        return $this->_filters['types'];
     }
 
     /**
@@ -64,6 +68,11 @@ class Manager
      */
     public function custom($name, $config = [])
     {
+        list($plugin, $filterClass) = pluginSplit($name);
+        if (!empty($plugin)) {
+            $this->_config['types'][$name] = '\\' . $plugin . '\Search\Type\\' . $filterClass;
+            return $this;
+        }
         if (isset($config['typeClasses'][$name])) {
             $this->_config['types'][$name] = new $config['typeClasses'][$name]($name, $config, $this);
             return $this;
@@ -76,6 +85,7 @@ class Manager
             $this->_config['types'][$name] = '\App\Search\Type\\' . $name;
             return $this;
         }
+        throw new \RuntimeException(sprintf('Can\'t find filter class "%s"!', $name));
     }
 
     public function __call($method, $args)
@@ -86,5 +96,79 @@ class Manager
             return $this;
         }
         return $this->custom($method, $args[0]);
+    }
+
+/**
+ * Gets all filters in a given collection.
+ *
+ * @param string $collection Name of the filter collection.
+ * @return array Array of filter instances.
+ */
+    public function getFilters($collection = 'default')
+    {
+        return $this->_filters[$collection];
+    }
+
+/**
+ * Sets or gets the filter collection name.
+ *
+ * @param string $name Name of the active filter collection to set.
+ * @return mixed Returns $this or the name of the active collection if no $name was provided.
+ */
+    public function collection($name = null)
+    {
+        if ($name === null) {
+            return $this->_collection;
+        }
+        if (!isset($this->_filters[$name])) {
+            $this->_filters[$name] = [];
+        }
+        $this->_collection = $name;
+        return $this;
+    }
+
+/**
+ * Adds a new filter to the active collection.
+ *
+ * @param string $name
+ * @param string $filter
+ * @param array $options
+ * @return $this
+ */
+    public function add($name, $filter, array $options = [])
+    {
+        $this->_filters[$this->_collection][$name] = $this->_loadFilter($filter, $options);
+        return $this;
+    }
+
+/**
+ * Loads a search filter instance.
+ *
+ * @param string $name Name of the filter class to load.
+ * @param array $options Filter options.
+ * @return \Search\Search\Type\Base
+ * @throws \InvalidArgumentException When no filter was found.
+ */
+    public function _loadFilter($name, array $options = [])
+    {
+        list($plugin, $name) = pluginSplit($name);
+        if (!empty($plugin)) {
+            $className = '\\' . $plugin . '\Search\Type\\' . $name;
+            if (class_exists($className)) {
+                return new $className($name, $options, $this);
+            }
+        }
+        if (isset($config['typeClasses'][$name])) {
+            return new $config['typeClasses'][$name]($name, $options, $this);
+        }
+        if (class_exists('\FOC\Search\Search\Type\\' . $name)) {
+            $className = '\FOC\Search\Search\Type\\' . $name;
+            return new $className($name, $options, $this);
+        }
+        if (class_exists('\App\Search\Type\\' . $name)) {
+            $className = '\App\Search\Type\\' . $name;
+            return new $className($name, $options, $this);
+        }
+        throw new \InvalidArgumentException(sprintf('Can\'t find filter class %s!', $name));
     }
 }
