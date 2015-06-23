52199be339109e72cd8c3ff6d6b5711917499ffc
ADmad <admad.coder@gmail.com>
2015-06-23 19:07:52 +0530

    Add tests

---
+++ tests/TestCase/Type/ValueTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace Search\Test\TestCase\Type;
+
+use Cake\Core\Configure;
+use Cake\ORM\TableRegistry;
+use Cake\ORM\Table;
+use Cake\ORM\Entity;
+use Cake\TestSuite\TestCase;
+use Search\Manager;
+use Search\Type\Base;
+use Search\Type\Value;
+
+class ValueTest extends TestCase
+{
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.Search.Articles'
+    ];
+
+    public function testProcess()
+    {
+        $articles = TableRegistry::get('Articles');
+        $manager = new Manager($articles);
+        $value = new Value('title', [], $manager);
+        $value->args(['title' => ['test']]);
+        $value->query($articles->find());
+        $result = $value->process();
+    }
+}
