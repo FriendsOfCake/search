1adecb98caa980a2b201c33b12ca898fccc42639
Florian Krämer <florian.kraemer@cakedc.com>
2015-12-16 13:28:07 +0100

    Made it easy to use PRG with just specific actions.
    It has been pretty annoying to repeat the action check over and over in the controllers. Let the component do this automatically for you now.

--- src/Controller/Component/PrgComponent.php
+++ src/Controller/Component/PrgComponent.php
@@ -2,26 +2,68 @@
 namespace Search\Controller\Component;
 
 use Cake\Controller\Component;
-use Cake\Event\Event;
 
-class PrgComponent extends Component
-{
+class PrgComponent extends Component {
+
+    /**
+     * Default config
+     *
+     * @var array
+     */
+    protected $_defaultConfig = [
+        'actions' => true,
+    ];
+
     /**
      * Checks if the current request has posted data and redirects the users
      * to the same action after converting the post data into GET params
      *
-     * @return void|Cake\Network\Response
+     * @return void|\Cake\Network\Response
      */
     public function startup()
     {
-        $controller = $this->_registry->getController();
-        $request = $controller->request;
+        $this->controller = $this->_registry->getController();
+        $this->request = $this->controller->request;
 
-        if (!$request->is('post')) {
-            $request->data = $request->query;
+        if ($this->_actionCheck()) {
+            $this->conversion();
+        }
+    }
+
+    /**
+     * POST to GET / GET to POST conversion
+     *
+     * @param bool $redirect Redirect on post, default true.
+     * @return \Cake\Network\Response|null
+     */
+    public function conversion($redirect = true)
+    {
+        if (!$this->request->is('post')) {
+            $this->request->data = $this->controller->request->query;
             return;
         }
+        if ($redirect) {
+            return $this->controller->redirect($this->request->params['pass'] + ['?' => $this->request->data]);
+        }
+    }
 
-        return $controller->redirect($request->params['pass'] + ['?' => $request->data]);
+    /**
+     * Checks if the action should be processed by the component.
+     *
+     * @return boolean
+     */
+    protected function _actionCheck()
+    {
+        $actions = $this->config('actions');
+        if (is_bool($actions)) {
+            return $actions;
+        }
+        if (is_string($actions)) {
+            $actions = [$actions];
+        }
+        if (in_array($this->request->action, $actions)) {
+            return true;
+        }
+        return false;
     }
 }
