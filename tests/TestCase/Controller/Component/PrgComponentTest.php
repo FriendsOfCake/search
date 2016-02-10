99cdd893fe53e48307efc9a0448539ddebbaa0fb
ADmad <admad.coder@gmail.com>
2016-02-11 00:55:49 +0530

    Improve redirection.
    Refs #67, #69.

--- tests/TestCase/Controller/Component/PrgComponentTest.php
+++ tests/TestCase/Controller/Component/PrgComponentTest.php
@@ -16,6 +16,11 @@ class SearchComponentTest extends TestCase
 
         Router::$initialized = true;
         Router::scope('/', function ($routes) {
+            $routes->connect(
+                '/users/my-predictions',
+                ['controller' => 'UserAnswers', 'action' => 'index', 'type' => 'open'],
+                ['pass' => ['type'], '_name' => 'userOpenPredictions']
+            );
             $routes->fallbacks();
         });
         $request = new Request();
@@ -37,15 +42,16 @@ class SearchComponentTest extends TestCase
     public function testInitializePost()
     {
         $this->Controller->request->params = [
-            'controller' => 'posts',
+            'controller' => 'Posts',
             'action' => 'index',
             'pass' => ['pass']
         ];
+        $this->Controller->request->here = '/Posts/index/pass';
         $this->Controller->request->data = ['foo' => 'bar'];
         $this->Controller->request->env('REQUEST_METHOD', 'POST');
 
         $response = $this->Prg->startup();
-        $this->assertEquals('http://localhost/index/pass?foo=bar', $response->header()['Location']);
+        $this->assertEquals('http://localhost/Posts/index/pass?foo=bar', $response->header()['Location']);
 
         $this->Prg->config('actions', false);
         $response = $this->Prg->startup();
@@ -56,15 +62,25 @@ class SearchComponentTest extends TestCase
         $this->assertEquals(null, $response);
 
         $this->Prg->config('actions', 'index', false);
+        $this->Controller->response->header('Location', '');
         $response = $this->Prg->startup();
-        $this->assertEquals('http://localhost/index/pass?foo=bar', $response->header()['Location']);
+        $this->assertEquals('http://localhost/Posts/index/pass?foo=bar', $response->header()['Location']);
 
         $this->Prg->config('actions', ['index', 'does-not-exist'], false);
+        $this->Controller->response->header('Location', '');
         $response = $this->Prg->startup();
-        $this->assertEquals('http://localhost/index/pass?foo=bar', $response->header()['Location']);
+        $this->assertEquals('http://localhost/Posts/index/pass?foo=bar', $response->header()['Location']);
 
-        $this->Prg->config('actions', ['does-not-exist'], false);
+        $this->Prg->config('actions', true);
+        $this->Controller->request->params = [
+            'controller' => 'UserAnswers',
+            'action' => 'index',
+            'type' => 'open',
+            'pass' => ['open']
+        ];
+        $this->Controller->request->here = '/users/my-predictions';
+        $this->Controller->response->header('Location', '');
         $response = $this->Prg->startup();
-        $this->assertEquals(null, $response);
+        $this->assertEquals('http://localhost/users/my-predictions?foo=bar', $response->header()['Location']);
     }
 }
