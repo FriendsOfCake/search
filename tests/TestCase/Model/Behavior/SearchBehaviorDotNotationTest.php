936aa0bd43e17223a5991d0d561115edb3bc39f1
Jonas Hartmann <jh@ht-studios.de>
2016-08-11 11:13:04 +0000

    unit tests work stand alone but clash due to class redeclare

---
+++ tests/TestCase/Model/Behavior/SearchBehaviorDotNotationTest.php
@@ -0,0 +1,77 @@
+<?php
+namespace Search\Test\TestCase\Model\Behavior;
+
+use Cake\ORM\Table;
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\TestCase;
+use Search\Manager;
+
+class ArticlesTable extends Table
+{
+
+    public function searchConfiguration()
+    {
+        $manager = new Manager($this);
+        return $manager
+            ->value('Articles.foo')
+            ->like('Articles.search', ['filterEmpty' => true])
+            ->value('Articles.baz')
+            ->value('Articles.group', ['field' => 'Articles.group']);
+    }
+}
+
+class SearchBehaviorTest extends TestCase
+{
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.Search.Articles'
+    ];
+
+    /**
+     * setup
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        TableRegistry::clear();
+        $this->Articles = TableRegistry::get('Articles', [
+            'className' => 'Search\Test\TestCase\Model\Behavior\ArticlesTable'
+        ]);
+        $this->Articles->addBehavior('Search.Search');
+    }
+
+    /**
+     * Test the custom "search" finder
+     *
+     * @return void
+     */
+    public function testFinder()
+    {
+        $queryString = [
+            'Articles' => [
+                'foo' => 'a',
+                'search' => 'b',
+                'group' => 'main'
+            ]
+        ];
+
+        $query = $this->Articles->find('search', ['search' => $queryString]);
+        $this->assertEquals(3, $query->clause('where')->count());
+
+        $queryString['Articles']['search'] = '';
+        $query = $this->Articles->find('search', ['search' => $queryString]);
+        $this->assertEquals(2, $query->clause('where')->count());
+
+        $queryString['Articles']['foo'] = '';
+        $query = $this->Articles->find('search', ['search' => $queryString]);
+        $this->assertEquals(1, $query->clause('where')->count());
+    }
+}
