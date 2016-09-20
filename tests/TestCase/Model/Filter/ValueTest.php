b8cdba4f9250345b52d568be2f4392d8287c3a5f
ndm2 <github-46j8925q79hg@nowak-media.de>
2016-09-20 03:57:58 +0200

    Add multi-value search support.
    refs #77

--- tests/TestCase/Model/Filter/ValueTest.php
+++ tests/TestCase/Model/Filter/ValueTest.php
@@ -1,13 +1,10 @@
 <?php
 namespace Search\Test\TestCase\Model\Filter;
 
-use Cake\Core\Configure;
-use Cake\ORM\Entity;
-use Cake\ORM\Table;
 use Cake\ORM\TableRegistry;
 use Cake\TestSuite\TestCase;
+use Cake\Utility\Hash;
 use Search\Manager;
-use Search\Model\Filter\Base;
 use Search\Model\Filter\Value;
 
 class ValueTest extends TestCase
@@ -26,9 +23,175 @@ class ValueTest extends TestCase
     {
         $articles = TableRegistry::get('Articles');
         $manager = new Manager($articles);
-        $value = new Value('title', $manager);
-        $value->args(['title' => ['test']]);
-        $value->query($articles->find());
-        $result = $value->process();
+        $filter = new Value('title', $manager);
+        $filter->args(['title' => 'test']);
+        $filter->query($articles->find());
+        $filter->process();
+
+        $this->assertRegExp(
+            '/WHERE Articles\.title = :c0$/',
+            $filter->query()->sql()
+        );
+        $this->assertEquals(
+            ['test'],
+            Hash::extract($filter->query()->valueBinder()->bindings(), '{s}.value')
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public function testProcessSingleValueWithAndMode()
+    {
+        $articles = TableRegistry::get('Articles');
+        $manager = new Manager($articles);
+        $filter = new Value('title', $manager, ['mode' => 'and']);
+        $filter->args(['title' => 'foo']);
+        $filter->query($articles->find());
+        $filter->process();
+
+        $this->assertRegExp(
+            '/WHERE Articles\.title = :c0$/',
+            $filter->query()->sql()
+        );
+        $this->assertEquals(
+            ['foo'],
+            Hash::extract($filter->query()->valueBinder()->bindings(), '{s}.value')
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public function testProcessMultiValue()
+    {
+        $articles = TableRegistry::get('Articles');
+        $manager = new Manager($articles);
+        $filter = new Value('title', $manager, ['multiValue' => true]);
+        $filter->args(['title' => ['foo', 'bar']]);
+        $filter->query($articles->find());
+        $filter->process();
+
+        $this->assertRegExp(
+            '/WHERE Articles\.title IN \(:c0,:c1\)$/',
+            $filter->query()->sql()
+        );
+        $this->assertEquals(
+            ['foo', 'bar'],
+            Hash::extract($filter->query()->valueBinder()->bindings(), '{s}.value')
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public function testProcessMultiValueWithAndMode()
+    {
+        $articles = TableRegistry::get('Articles');
+        $manager = new Manager($articles);
+        $filter = new Value('title', $manager, [
+            'multiValue' => true,
+            'mode' => 'and'
+        ]);
+        $filter->args(['title' => ['foo', 'bar']]);
+        $filter->query($articles->find());
+        $filter->process();
+
+        $this->assertRegExp(
+            '/WHERE \(Articles\.title = :c0 AND Articles\.title = :c1\)$/',
+            $filter->query()->sql()
+        );
+        $this->assertEquals(
+            ['foo', 'bar'],
+            Hash::extract($filter->query()->valueBinder()->bindings(), '{s}.value')
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public function testProcessMultiValueWithNonScalarValue()
+    {
+        $articles = TableRegistry::get('Articles');
+        $manager = new Manager($articles);
+        $filter = new Value('title', $manager, ['multiValue' => true]);
+        $filter->args(['title' => ['foo' => ['bar']]]);
+        $filter->query($articles->find());
+        $filter->process();
+
+        $this->assertRegExp(
+            '/WHERE Articles\.title IN \(:c0\)$/',
+            $filter->query()->sql()
+        );
+        $this->assertEquals(
+            [['bar']],
+            Hash::extract($filter->query()->valueBinder()->bindings(), '{s}.value')
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public function testProcessDefaultFallbackForDisallowedMultiValue()
+    {
+        $articles = TableRegistry::get('Articles');
+        $manager = new Manager($articles);
+        $filter = new Value('title', $manager, ['defaultValue' => 'default']);
+        $filter->args(['title' => ['foo', 'bar']]);
+        $filter->query($articles->find());
+        $filter->process();
+
+        $this->assertRegExp(
+            '/WHERE Articles\.title = :c0$/',
+            $filter->query()->sql()
+        );
+        $this->assertEquals(
+            ['default'],
+            Hash::extract($filter->query()->valueBinder()->bindings(), '{s}.value')
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public function testProcessNoDefaultFallbackForDisallowedMultiValue()
+    {
+        $articles = TableRegistry::get('Articles');
+        $manager = new Manager($articles);
+        $filter = new Value('title', $manager);
+        $filter->args(['title' => ['foo', 'bar']]);
+        $filter->query($articles->find());
+        $filter->process();
+
+        $this->assertNotRegExp(
+            '/Articles\.title like/',
+            $filter->query()->sql()
+        );
+        $this->assertEmpty($filter->query()->valueBinder()->bindings());
+    }
+
+    /**
+     * @return void
+     */
+    public function testProcessCaseInsensitiveMode()
+    {
+        $articles = TableRegistry::get('Articles');
+        $manager = new Manager($articles);
+        $filter = new Value('title', $manager, [
+            'multiValue' => true,
+            'mode' => 'Or'
+        ]);
+        $filter->args(['title' => ['foo', 'bar']]);
+        $filter->query($articles->find());
+        $filter->process();
+
+        $this->assertRegExp(
+            '/WHERE Articles\.title IN \(:c0,:c1\)$/',
+            $filter->query()->sql()
+        );
+        $this->assertEquals(
+            ['foo', 'bar'],
+            Hash::extract($filter->query()->valueBinder()->bindings(), '{s}.value')
+        );
     }
 }
