751f0ccdaecec85ead9173349531feeb0c39d4ba
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2019-11-29 23:09:45 -0500

    feat: create Plugin class

---
+++ src/Plugin.php
@@ -0,0 +1,17 @@
+<?php
+namespace Search;
+
+use Cake\Core\BasePlugin;
+
+/**
+ * Plugin class for Search
+ */
+class Plugin extends BasePlugin
+{
+    /**
+     * Plugin name.
+     *
+     * @var string
+     */
+    protected $name = 'Search';
+}
