6a58a223aefd9c7314e994ee17cb5dbc5c6e2785
ADmad <admad.coder@gmail.com>
2020-11-24 20:12:06 +0530

    Fix error when bake plugin is not loaded.
    Closes #305

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Search;
 
+use Cake\Console\CommandCollection;
 use Cake\Core\BasePlugin;
 
 class Plugin extends BasePlugin
@@ -27,4 +28,17 @@ class Plugin extends BasePlugin
      * @var bool
      */
     protected $routesEnabled = false;
+
+    /**
+     * Add console commands for the plugin.
+     *
+     * @param \Cake\Console\CommandCollection $commands The command collection to update
+     * @return \Cake\Console\CommandCollection
+     */
+    public function console(CommandCollection $commands): CommandCollection
+    {
+        // Bake plugin handles discovery of bake commands itself.
+        // Since we currently only have a command class for bake command, we don't need to to anything here.
+        return $commands;
+    }
 }
